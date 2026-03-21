65fb1d8db03a19682dd682c45c1a0294c963682a
SecretChilliZ <hopeofthehopeless@protonmail.com>
2026-03-21 09:32:22 +0800

    Initial commit
    # Conflicts:
    #	.gitignore
    #	.idea/deploymentTargetSelector.xml
    #	.idea/gradle.xml
    #	.idea/misc.xml
    #	app/build.gradle.kts
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/drawable/ic_launcher_background.xml
    #	app/src/main/res/drawable/ic_launcher_foreground.xml
    #	app/src/main/res/mipmap-anydpi/ic_launcher.xml
    #	app/src/main/res/mipmap-anydpi/ic_launcher_round.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/themes.xml
    #	build.gradle.kts
    #	gradle.properties
    #	gradle/libs.versions.toml
    #	gradle/wrapper/gradle-wrapper.properties
    #	settings.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,24 +1,24 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
 
 android {
-    namespace = "com.chilliz.stimulant"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    namespace = "com.chilliz.hardwaremacro"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.chilliz.stimulant"
+        applicationId = "com.chilliz.hardwaremacro"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -31,8 +31,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
     buildFeatures {
         compose = true
@@ -48,7 +51,12 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.api)
+    implementation(libs.androidx.appcompat)
+    
+    // Shizuku
+    implementation("dev.rikka.shizuku:api:13.1.5")
+    implementation("dev.rikka.shizuku:provider:13.1.5")
+    
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,5 +22,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Stimulant"
+rootProject.name = "HardwareMacro"
 include(":app")
+ 
